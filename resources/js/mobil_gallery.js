a3c4445b4977e359fb7d8eecab51c86be3df26ac
Omojide Oluwafemi <omojide.o.j@gmail.com>
2025-03-09 20:45:23 +0100

    add new gallery script

--- resources/js/mobil_gallery.js
+++ resources/js/mobil_gallery.js
@@ -1,10 +1,13 @@
-function openModal(title, description, image) {
+function openModal(title, description, image, liveLink, githubLink) {
     document.getElementById('modalTitle').textContent = title;
     document.getElementById('modalDescription').textContent = description;
     document.getElementById('modalImage').src = image;
+    document.getElementById('liveDemo').href = liveLink;
+    document.getElementById('githubRepo').href = githubLink;
     document.getElementById('projectModal').style.display = "block";
 }
 
+// Close Modal
 function closeModal() {
     document.getElementById('projectModal').style.display = "none";
 }
